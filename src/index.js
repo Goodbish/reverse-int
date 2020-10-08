6634bf249109fc1b8bb5ea3bfb5f0e944a0a6f5e
Goodbish <43109002+Goodbish@users.noreply.github.com>
2020-10-08 17:38:18 +0500

    init: start reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (num) {
+    if (num < 0) { num = Math.abs(num) }
+    num = +num.toString().split('').reverse().join(''); 
+  return num
 }
